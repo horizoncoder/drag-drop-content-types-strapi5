1660faee2a926baf59a07c93581e46bf7fcb0b0a
Emre Kara <yunusemrejs@outlook.com>
2025-02-11 18:55:55 +0300

    feat: update plugin ID and version to 1.0.2 for drag-drop-content-types-strapi5

--- admin/src/pluginId.ts
+++ admin/src/pluginId.ts
@@ -1,1 +1,1 @@
-export const PLUGIN_ID = 'drag-drop-content-types';
+export const PLUGIN_ID = 'drag-drop-content-types-strapi5';
--- server/src/services/settings.ts
+++ server/src/services/settings.ts
@@ -2,45 +2,42 @@ import type { Core } from '@strapi/strapi';
 import { PluginSettingsResponse } from '../../../typings';
 
 const getPluginStore = () => {
-    return strapi.store({
-        environment: '',
-        type: 'plugin',
-        name: 'drag-drop-content-types',
-    });
+  return strapi.store({
+    environment: '',
+    type: 'plugin',
+    name: 'drag-drop-content-types-strapi5',
+  });
 };
 
 const createDefaultConfig = async () => {
-    const pluginStore = getPluginStore();
-    const value: PluginSettingsResponse = {
-        body: {
-            rank: 'rank',
-            title: '',
-            subtitle: '',
-            triggerWebhooks: false,
-        },
-    };
-    ;
-    await pluginStore.set({ key: 'settings', value });
-    return pluginStore.get({ key: 'settings' });
+  const pluginStore = getPluginStore();
+  const value: PluginSettingsResponse = {
+    body: {
+      rank: 'rank',
+      title: '',
+      subtitle: '',
+      triggerWebhooks: false,
+    },
+  };
+  await pluginStore.set({ key: 'settings', value });
+  return pluginStore.get({ key: 'settings' });
 };
 
 export default ({ strapi }: { strapi: Core.Strapi }) => ({
+  async getSettings() {
+    const pluginStore = getPluginStore();
+    let config = await pluginStore.get({ key: 'settings' });
+    if (!config) {
+      config = await createDefaultConfig();
+    }
+    return config;
+  },
 
-    async getSettings() {
-        const pluginStore = getPluginStore();
-        let config = await pluginStore.get({ key: 'settings' });
-        if (!config) {
-            config = await createDefaultConfig();
-        }
-        return config;
-    },
+  async setSettings(settings) {
+    const value = settings;
+    const pluginStore = getPluginStore();
 
-    async setSettings(settings) {
-        const value = settings;
-        const pluginStore = getPluginStore();
-
-        await pluginStore.set({ key: 'settings', value });
-        return pluginStore.get({ key: 'settings' });
-    },
-
+    await pluginStore.set({ key: 'settings', value });
+    return pluginStore.get({ key: 'settings' });
+  },
 });
