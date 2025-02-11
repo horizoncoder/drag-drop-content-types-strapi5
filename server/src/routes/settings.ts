3f1c5c63fed63753c4d4a8d2e1c4b7c930cc4429
Emre Kara <yunusemrejs@outlook.com>
2025-02-11 20:35:45 +0300

    refactor: remove authentication policies from settings routes

--- server/src/routes/settings.ts
+++ server/src/routes/settings.ts
@@ -6,15 +6,7 @@ export default {
       path: '/settings',
       handler: 'settings.getSettings',
       config: {
-        policies: [
-          'strapi::isAuthenticatedAdmin',
-          {
-            name: 'admin::hasPermissions',
-            config: {
-              permissions: ['plugin::drag-drop-content-types-strapi5.read'],
-            },
-          },
-        ],
+        policies: [],
       },
     },
     {
@@ -22,15 +14,7 @@ export default {
       path: '/settings',
       handler: 'settings.setSettings',
       config: {
-        policies: [
-          'strapi::isAuthenticatedAdmin',
-          {
-            name: 'admin::hasPermissions',
-            config: {
-              permissions: ['plugin::drag-drop-content-types-strapi5.settings.update'],
-            },
-          },
-        ],
+        policies: [],
       },
     },
   ],
